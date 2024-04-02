212371cb5a17614990d86e342b5d70863a5b9cc7
Binod Manandhar <binodmanandhar7@gmail.com>
2024-04-03 09:32:52 +1100

    Create post completed

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,12 +1,120 @@
-import React from "react";
+// "use client";
+// import { useRouter } from "next/navigation";
+// import React, { ChangeEvent, useState } from "react";
+
+// const Form = () => {
+//   const [formData, setFormData] = useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     title: "",
+//     description: "",
+//   });
+
+//   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+
+//     console.log(setFormData);
+//   };
+
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     // Do something with the form data, like submitting it to an API
+//     console.log("Form data:", formData);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         name="firstName"
+//         value={formData.firstName}
+//         onChange={handleInputChange}
+//         placeholder="First Name"
+//       />
+//       <input
+//         type="text"
+//         name="lastName"
+//         value={formData.lastName}
+//         onChange={handleInputChange}
+//         placeholder="Last Name"
+//       />
+//       <input
+//         type="email"
+//         name="email"
+//         value={formData.email}
+//         onChange={handleInputChange}
+//         placeholder="Email"
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// };
+
+// export default Form;
+
+"use client";
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, useState } from "react";
 
 const CreatePage = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<String | null>(null);
+  const router = useRouter();
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("form data");
+    if (!formData.title || !formData.description) {
+      setError("Please fill in all the fields");
+      return;
+    }
+    setError(null);
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("api/jobs", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create job");
+      }
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+      setError("Something went wrong. Please try again!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <>
       <div className="heading text-3xl text-indigo-800 font-bold mb-8">
         Create post
       </div>
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit}>
         <div className="mb-5">
           <div className="mb-7">
             <label
@@ -18,9 +126,11 @@ const CreatePage = () => {
             <input
               type="text"
               id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleInputChange}
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               placeholder="title"
-              required
             />
           </div>
 
@@ -33,19 +143,23 @@ const CreatePage = () => {
 
           <textarea
             id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             placeholder="Write your thoughts here..."
-            required
           />
         </div>
 
         <button
           type="submit"
+          disabled={isLoading}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Save
+          {isLoading ? "Saving..." : "Save"}
         </button>
       </form>
+      {error && <p className="mt-4 text-red-500">{error}</p>}
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 // Define the interface for the object containing title and description.
-interface IJobs {
+interface IJob {
   $id: string;
   title: string;
   description: string;
@@ -13,7 +13,7 @@ interface IJobs {
 
 export default function Home() {
   // State to store fetched data
-  const [jobs, setJobs] = useState<IJobs[]>([]);
+  const [jobs, setJobs] = useState<IJob[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   useEffect(() => {
@@ -36,12 +36,21 @@ export default function Home() {
     getJobs();
   }, []);
 
+  const handleDelete = async (id: string) => {
+    try {
+      setJobs((prevJobs) => prevJobs?.filter((item) => item.$id != id));
+      await fetch(`/api/jobs/${id}`, { method: "DELETE" });
+    } catch (error) {
+      setError("Failed to delete the jobs. Please try again");
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <div>{error && <p className="py-4 text-red-500">{error}</p>}</div>
       {isLoading ? (
         <p>Loading Jobs...</p>
-      ) : (
+      ) : jobs?.length > 0 ? (
         <div>
           {jobs.map((job, index) => (
             <div
@@ -55,20 +64,23 @@ export default function Home() {
               <div className="mt-8 flex justify-end gap-4">
                 <Link
                   className="inline-block text-white bg-blue-600 from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  href={"/edit"}
+                  href={`/edit/${job.$id}`}
                 >
                   Edit
                 </Link>
-                <Link
+                <button
+                  type="button"
+                  onClick={() => handleDelete(job.$id)}
                   className="inline-block text-white bg-red-600 from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  href={"/delete"}
                 >
                   Delete
-                </Link>
+                </button>
               </div>
             </div>
           ))}
         </div>
+      ) : (
+        <p>No Jobs found!</p>
       )}
     </div>
   );
